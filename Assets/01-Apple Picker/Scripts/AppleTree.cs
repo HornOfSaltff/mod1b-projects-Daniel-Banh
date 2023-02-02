ce9243119c48168e9fd4a3f06ffed5fcd4fcc924
Daniel-Banh <104493694+Daniel-Banh@users.noreply.github.com>
2023-02-02 18:51:21 -0500

    Apple Tree Random Movement Script

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -9,16 +9,16 @@ public class AppleTree : MonoBehaviour
     public GameObject applePrefab;
 
     // Speed at which the AppleTree moves
-    public float speed = 1f;
+    public float speed = 10f;
 
     // Distance where AppleTree turns around
-    public float leftAndRightEdge = 10f;
+    public float leftAndRightEdge = 20f;
 
-    // Chance that the AppleTree will change di
-    public float chanceToChangeDirection
+    // Chance that the AppleTree will change direction
+    public float chanceToChangeDirection;
 
     // Rate at which Apples will be instantiate
-    public float secondsBetweenAppleDrop
+    public float secondsBetweenAppleDrop;
 
     void Start()
     {
@@ -28,6 +28,24 @@ public class AppleTree : MonoBehaviour
     void Update()
     {
         // Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
         // Changing Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); // Move right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); // Move left
+        }
+    }
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirection)
+        {
+            speed *= -1; //Change Direction
+        }
     }
 }
